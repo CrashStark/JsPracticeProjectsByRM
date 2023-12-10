b0ecee601edc5d0a093b93b492d13031e68a8590
CrashStark <starkcrash0@gmail.com>
2023-12-11 01:58:05 +0530

    Completed Crypto App

---
+++ CryptoWebbb/script.js
@@ -0,0 +1,16 @@
+var settings = {
+  async: true,
+  scrossDomain: true,
+  url: "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereun%2Cdogecoin&vs_currencies=USD",
+  method: "GET",
+  headers: {},
+};
+let btc = document.getElementById("bitcoin");
+let eth = document.getElementById("ethereum");
+let doge = document.getElementById("dogecoin");
+$.ajax(settings).done(function (response) {
+  console.log(response);
+  btc.innerHTML = response.bitcoin.usd;
+//   eth.innerHTML = response.ethereum.usd;
+  doge.innerHTML = response.dogecoin.usd;
+});
